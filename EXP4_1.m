a17c89a1623d3c8211b892b32097a6cfc318f6a3
zhaoguangyu <690751687@qq.com>
2022-05-07 16:29:35 +0800

    remove plot part in th code

--- EXP4_1.m
+++ EXP4_1.m
@@ -15,19 +15,9 @@ x = sin(1e3*2*pi*t)+sin(2e3*2*pi*t)+sin(3e3*2*pi*t)+normrnd(0,1,1,N);
 x_mean = mean(x); % 均值
 x2_mean = mean(x.^2); % 均方值
 [R_x,lag] = xcorr(x,'unbiased'); % 自相关函数
-figure(1);
-plot(lag,R_x);
 [p_x,xi] = ksdensity(x); % 概率密度
 X = fft(x); % 频谱
-figure(2)
-plotFFT(X,fs);
 G_x = fft(R_x); % 功率谱密度
-figure(3)
-[psd_a,psd_fa] = pwelch(x,N/2,40,N,fs);
-subplot(2,1,1)
-plotFFT(G_x,fs);
-subplot(2,1,2)
-plot(psd_fa,psd_a)
 
 
 %wp和ws分别是通带和阻带的频率(截止频率)。当wp和ws为二元矢量时，为带通或带阻滤波器，这时求出的Wn也是二元矢量；当wp和ws为一元矢量时，为低通或高通滤波器：当wp<ws时为低通滤波器，当wp>ws时为高通滤波器。
@@ -54,11 +44,6 @@ impz(b,a);                      % 滤波器的单位冲击响应
 
 figure(6)
 x_a=filter(b,a,x);                 % 经过带通滤波器之后的时域波形
-subplot(2,1,1);
-plot(t(1:300),x_a(1:300));
-xlabel("t/s")
-subplot(2,1,2);
-plotFFT(fft(x_a),fs);
 
 % 限幅器直接使用循环实现
 y_1 = zeros(1,max(size(x_a)));
@@ -75,41 +60,11 @@ for i = 1:max(size(x_a))
     end
 end
 
-% 对比限幅器前后时域与频域
-figure(7)
-subplot(2,1,1)
-plot(t(1:300),x_a(1:300),'r')
-hold on
-plot(t(1:300),y_1(1:300),'g')
-subplot(2,2,3)
-plotFFT(fft(x_a),fs);
-subplot(2,2,4)
-plotFFT(fft(y_1),fs);
-
 % 平方律
 x_b = zeros(1,max(size(x)));
 for i = 1:max(size(x))
     x_b(i) = squareLaw(x(i));
 end
-
-figure(8)
-subplot(2,1,1)
-plot(t(1:700),x(1:700),'r')
-hold on
-plot(t(1:700),x_b(1:700),'g')
-subplot(2,2,3)
-plotFFT(fft(x),fs);
-subplot(2,2,4)
-plotFFT(fft(x_b),fs);
-
-figure(9)
-y_2 = filter(b,a,x_b);
-subplot(2,1,1)
-plot(y_2)
-subplot(2,1,2)
-plotFFT(fft(y_2),fs);
-
-
 
 pause()
 close all;
