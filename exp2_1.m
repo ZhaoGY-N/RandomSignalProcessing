61d7873fafb4cf4545594392bab24d85650270e3
zhaoguangyu <690751687@qq.com>
2022-05-09 20:04:40 +0800

    实验二基本完成

--- exp2_1.m
+++ exp2_1.m
@@ -20,10 +20,12 @@ b = max(temp)+3*sig;
 % 计算均值
 f_ex = @(x)x.*p(x);
 mx = mcIntxy(f_ex,N,a,b);
+fprintf("均值为：%f\n",mx);
 
 % 计算方差
 f_var = @(x)(x-mx).^2.*p(x);
 varx = mcIntxy(f_var,N,a,b);
+fprintf("方差为：%f\n",varx);
 
 
 % 计算协方差，当t1 neq t2时，我们可以得到
@@ -32,6 +34,10 @@ Cx1 = mcIntxyz(f_Cx,N,a,b,a,b);
 % 当t1 = t2时，我们有 x1 = x2，一定成立
 f_Cx = @(x)(x-mx).^2.*p(x); % 与f_var相等
 Cx2 = mcIntxy(f_Cx,N,a,b);
+fprintf("当t1 = t2时，协方差为：%f\n",Cx2);
+fprintf("当t1 不等于 t2时，协方差为：%f\n",Cx1);
+
+
 
 % 计算相关函数，当t1 neq t2时，我们可以得到
 f_Rx = @(x1,x2)x1.*x2.*p(x1).*p(x2);
@@ -39,3 +45,5 @@ Rx1 = mcIntxyz(f_Rx,N,a,b,a,b);
 % 当t1 = t2时，我们有 x1 = x2，一定成立
 f_Rx = @(x)x.^2.*p(x);
 Rx2 = mcIntxy(f_Rx,N,a,b);
+fprintf("当t1 = t2时，相关函数为：%f\n",Rx2);
+fprintf("当t1 不等于 t2时，相关函数为：%f\n",Rx1);
--- exp2_2.m
+++ exp2_2.m
@@ -9,8 +9,17 @@ p = unifrnd(0,1,1,N);
 
 x = norminv(p,mu,sig);
 E_x = mean(x);
+fprintf("均值为：%f\n",E_x);
+
 Var_x = var(x);
+fprintf("方差为：%f\n",Var_x);
 
 [R_x,lag]=xcorr(x,'unbiased');
+figure(1)
 plot(lag,R_x);
+xlim([round(-N*0.7),round(N*0.7)])
+
+C_x = R_x - E_x*E_x;
+figure(2)
+plot(lag,C_x);
 xlim([round(-N*0.7),round(N*0.7)])
